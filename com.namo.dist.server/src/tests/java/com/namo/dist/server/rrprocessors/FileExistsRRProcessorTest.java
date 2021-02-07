3372027aa0d328e24d88da38ac2f36ba5a5fb01d
priyanka kasliwal <pkasliwal@gmail.com>
2021-02-06 22:09:02 -0700

    Updated project structure and added build steps

---
+++ com.namo.dist.server/src/tests/java/com/namo/dist/server/rrprocessors/FileExistsRRProcessorTest.java
@@ -0,0 +1,14 @@
+package com.namo.dist.server.rrprocessors;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class FileExistsRRProcessorTest {
+
+	private FileExistsRRProcessor processorToTest = new FileExistsRRProcessor();
+	
+	@Test
+	public void testProcessor() {
+		Assert.assertTrue(true);
+	}
+}
